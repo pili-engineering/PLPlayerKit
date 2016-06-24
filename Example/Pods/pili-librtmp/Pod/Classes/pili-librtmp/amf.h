e333b2be523e28f40c8dde9fa12a654f029c752f
liangliang <liangliang0918@126.com>
2016-06-24 18:50:14 +0800

    update pili-librtmp to 1.0.3.

--- Example/Pods/pili-librtmp/Pod/Classes/pili-librtmp/amf.h
+++ Example/Pods/pili-librtmp/Pod/Classes/pili-librtmp/amf.h
@@ -28,134 +28,153 @@
 #include <stdint.h>
 
 #ifndef TRUE
-#define TRUE	1
-#define FALSE	0
+#define TRUE 1
+#define FALSE 0
 #endif
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-  typedef enum
-  { AMF_NUMBER = 0, AMF_BOOLEAN, AMF_STRING, AMF_OBJECT,
-    AMF_MOVIECLIP,		/* reserved, not used */
-    AMF_NULL, AMF_UNDEFINED, AMF_REFERENCE, AMF_ECMA_ARRAY, AMF_OBJECT_END,
-    AMF_STRICT_ARRAY, AMF_DATE, AMF_LONG_STRING, AMF_UNSUPPORTED,
-    AMF_RECORDSET,		/* reserved, not used */
-    AMF_XML_DOC, AMF_TYPED_OBJECT,
-    AMF_AVMPLUS,		/* switch to AMF3 */
+typedef enum {
+    AMF_NUMBER = 0,
+    AMF_BOOLEAN,
+    AMF_STRING,
+    AMF_OBJECT,
+    AMF_MOVIECLIP, /* reserved, not used */
+    AMF_NULL,
+    AMF_UNDEFINED,
+    AMF_REFERENCE,
+    AMF_ECMA_ARRAY,
+    AMF_OBJECT_END,
+    AMF_STRICT_ARRAY,
+    AMF_DATE,
+    AMF_LONG_STRING,
+    AMF_UNSUPPORTED,
+    AMF_RECORDSET, /* reserved, not used */
+    AMF_XML_DOC,
+    AMF_TYPED_OBJECT,
+    AMF_AVMPLUS, /* switch to AMF3 */
     AMF_INVALID = 0xff
-  } AMFDataType;
+} AMFDataType;
 
-  typedef enum
-  { AMF3_UNDEFINED = 0, AMF3_NULL, AMF3_FALSE, AMF3_TRUE,
-    AMF3_INTEGER, AMF3_DOUBLE, AMF3_STRING, AMF3_XML_DOC, AMF3_DATE,
-    AMF3_ARRAY, AMF3_OBJECT, AMF3_XML, AMF3_BYTE_ARRAY
-  } AMF3DataType;
+typedef enum {
+    AMF3_UNDEFINED = 0,
+    AMF3_NULL,
+    AMF3_FALSE,
+    AMF3_TRUE,
+    AMF3_INTEGER,
+    AMF3_DOUBLE,
+    AMF3_STRING,
+    AMF3_XML_DOC,
+    AMF3_DATE,
+    AMF3_ARRAY,
+    AMF3_OBJECT,
+    AMF3_XML,
+    AMF3_BYTE_ARRAY
+} AMF3DataType;
 
-  typedef struct AVal
-  {
+typedef struct AVal {
     char *av_val;
     int av_len;
-  } AVal;
-#define AVC(str)	{str,sizeof(str)-1}
-#define AVMATCH(a1,a2)	((a1)->av_len == (a2)->av_len && !memcmp((a1)->av_val,(a2)->av_val,(a1)->av_len))
+} AVal;
+#define AVC(str) \
+    { str, sizeof(str) - 1 }
+#define AVMATCH(a1, a2)              \
+    ((a1)->av_len == (a2)->av_len && \
+     !memcmp((a1)->av_val, (a2)->av_val, (a1)->av_len))
 
-  struct AMFObjectProperty;
+struct AMFObjectProperty;
 
-  typedef struct AMFObject
-  {
+typedef struct AMFObject {
     int o_num;
     struct AMFObjectProperty *o_props;
-  } AMFObject;
+} AMFObject;
 
-  typedef struct AMFObjectProperty
-  {
+typedef struct AMFObjectProperty {
     AVal p_name;
     AMFDataType p_type;
-    union
-    {
-      double p_number;
-      AVal p_aval;
-      AMFObject p_object;
+    union {
+        double p_number;
+        AVal p_aval;
+        AMFObject p_object;
     } p_vu;
     int16_t p_UTCoffset;
-  } AMFObjectProperty;
+} AMFObjectProperty;
 
-  char *AMF_EncodeString(char *output, char *outend, const AVal * str);
-  char *AMF_EncodeNumber(char *output, char *outend, double dVal);
-  char *AMF_EncodeInt16(char *output, char *outend, short nVal);
-  char *AMF_EncodeInt24(char *output, char *outend, int nVal);
-  char *AMF_EncodeInt32(char *output, char *outend, int nVal);
-  char *AMF_EncodeBoolean(char *output, char *outend, int bVal);
+char *AMF_EncodeString(char *output, char *outend, const AVal *str);
+char *AMF_EncodeNumber(char *output, char *outend, double dVal);
+char *AMF_EncodeInt16(char *output, char *outend, short nVal);
+char *AMF_EncodeInt24(char *output, char *outend, int nVal);
+char *AMF_EncodeInt32(char *output, char *outend, int nVal);
+char *AMF_EncodeBoolean(char *output, char *outend, int bVal);
 
-  /* Shortcuts for AMFProp_Encode */
-  char *AMF_EncodeNamedString(char *output, char *outend, const AVal * name, const AVal * value);
-  char *AMF_EncodeNamedNumber(char *output, char *outend, const AVal * name, double dVal);
-  char *AMF_EncodeNamedBoolean(char *output, char *outend, const AVal * name, int bVal);
+/* Shortcuts for AMFProp_Encode */
+char *AMF_EncodeNamedString(char *output, char *outend, const AVal *name,
+                            const AVal *value);
+char *AMF_EncodeNamedNumber(char *output, char *outend, const AVal *name,
+                            double dVal);
+char *AMF_EncodeNamedBoolean(char *output, char *outend, const AVal *name,
+                             int bVal);
 
-  unsigned short AMF_DecodeInt16(const char *data);
-  unsigned int AMF_DecodeInt24(const char *data);
-  unsigned int AMF_DecodeInt32(const char *data);
-  void AMF_DecodeString(const char *data, AVal * str);
-  void AMF_DecodeLongString(const char *data, AVal * str);
-  int AMF_DecodeBoolean(const char *data);
-  double AMF_DecodeNumber(const char *data);
+unsigned short AMF_DecodeInt16(const char *data);
+unsigned int AMF_DecodeInt24(const char *data);
+unsigned int AMF_DecodeInt32(const char *data);
+void AMF_DecodeString(const char *data, AVal *str);
+void AMF_DecodeLongString(const char *data, AVal *str);
+int AMF_DecodeBoolean(const char *data);
+double AMF_DecodeNumber(const char *data);
 
-  char *AMF_Encode(AMFObject * obj, char *pBuffer, char *pBufEnd);
-  int AMF_Decode(AMFObject * obj, const char *pBuffer, int nSize,
-		 int bDecodeName);
-  int AMF_DecodeArray(AMFObject * obj, const char *pBuffer, int nSize,
-		      int nArrayLen, int bDecodeName);
-  int AMF3_Decode(AMFObject * obj, const char *pBuffer, int nSize,
-		  int bDecodeName);
-  void AMF_Dump(AMFObject * obj);
-  void AMF_Reset(AMFObject * obj);
+char *AMF_Encode(AMFObject *obj, char *pBuffer, char *pBufEnd);
+int AMF_Decode(AMFObject *obj, const char *pBuffer, int nSize, int bDecodeName);
+int AMF_DecodeArray(AMFObject *obj, const char *pBuffer, int nSize,
+                    int nArrayLen, int bDecodeName);
+int AMF3_Decode(AMFObject *obj, const char *pBuffer, int nSize,
+                int bDecodeName);
+void AMF_Dump(AMFObject *obj);
+void AMF_Reset(AMFObject *obj);
 
-  void AMF_AddProp(AMFObject * obj, const AMFObjectProperty * prop);
-  int AMF_CountProp(AMFObject * obj);
-  AMFObjectProperty *AMF_GetProp(AMFObject * obj, const AVal * name,
-				 int nIndex);
+void AMF_AddProp(AMFObject *obj, const AMFObjectProperty *prop);
+int AMF_CountProp(AMFObject *obj);
+AMFObjectProperty *AMF_GetProp(AMFObject *obj, const AVal *name, int nIndex);
 
-  AMFDataType AMFProp_GetType(AMFObjectProperty * prop);
-  void AMFProp_SetNumber(AMFObjectProperty * prop, double dval);
-  void AMFProp_SetBoolean(AMFObjectProperty * prop, int bflag);
-  void AMFProp_SetString(AMFObjectProperty * prop, AVal * str);
-  void AMFProp_SetObject(AMFObjectProperty * prop, AMFObject * obj);
+AMFDataType AMFProp_GetType(AMFObjectProperty *prop);
+void AMFProp_SetNumber(AMFObjectProperty *prop, double dval);
+void AMFProp_SetBoolean(AMFObjectProperty *prop, int bflag);
+void AMFProp_SetString(AMFObjectProperty *prop, AVal *str);
+void AMFProp_SetObject(AMFObjectProperty *prop, AMFObject *obj);
 
-  void AMFProp_GetName(AMFObjectProperty * prop, AVal * name);
-  void AMFProp_SetName(AMFObjectProperty * prop, AVal * name);
-  double AMFProp_GetNumber(AMFObjectProperty * prop);
-  int AMFProp_GetBoolean(AMFObjectProperty * prop);
-  void AMFProp_GetString(AMFObjectProperty * prop, AVal * str);
-  void AMFProp_GetObject(AMFObjectProperty * prop, AMFObject * obj);
+void AMFProp_GetName(AMFObjectProperty *prop, AVal *name);
+void AMFProp_SetName(AMFObjectProperty *prop, AVal *name);
+double AMFProp_GetNumber(AMFObjectProperty *prop);
+int AMFProp_GetBoolean(AMFObjectProperty *prop);
+void AMFProp_GetString(AMFObjectProperty *prop, AVal *str);
+void AMFProp_GetObject(AMFObjectProperty *prop, AMFObject *obj);
 
-  int AMFProp_IsValid(AMFObjectProperty * prop);
+int AMFProp_IsValid(AMFObjectProperty *prop);
 
-  char *AMFProp_Encode(AMFObjectProperty * prop, char *pBuffer, char *pBufEnd);
-  int AMF3Prop_Decode(AMFObjectProperty * prop, const char *pBuffer,
-		      int nSize, int bDecodeName);
-  int AMFProp_Decode(AMFObjectProperty * prop, const char *pBuffer,
-		     int nSize, int bDecodeName);
+char *AMFProp_Encode(AMFObjectProperty *prop, char *pBuffer, char *pBufEnd);
+int AMF3Prop_Decode(AMFObjectProperty *prop, const char *pBuffer, int nSize,
+                    int bDecodeName);
+int AMFProp_Decode(AMFObjectProperty *prop, const char *pBuffer, int nSize,
+                   int bDecodeName);
 
-  void AMFProp_Dump(AMFObjectProperty * prop);
-  void AMFProp_Reset(AMFObjectProperty * prop);
+void AMFProp_Dump(AMFObjectProperty *prop);
+void AMFProp_Reset(AMFObjectProperty *prop);
 
-  typedef struct AMF3ClassDef
-  {
+typedef struct AMF3ClassDef {
     AVal cd_name;
     char cd_externalizable;
     char cd_dynamic;
     int cd_num;
     AVal *cd_props;
-  } AMF3ClassDef;
+} AMF3ClassDef;
 
-  void AMF3CD_AddProp(AMF3ClassDef * cd, AVal * prop);
-  AVal *AMF3CD_GetProp(AMF3ClassDef * cd, int idx);
+void AMF3CD_AddProp(AMF3ClassDef *cd, AVal *prop);
+AVal *AMF3CD_GetProp(AMF3ClassDef *cd, int idx);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif				/* __AMF_H__ */
+#endif /* __AMF_H__ */
--- Example/Pods/pili-librtmp/Pod/Classes/pili-librtmp/rtmp_sys.h
+++ Example/Pods/pili-librtmp/Pod/Classes/pili-librtmp/rtmp_sys.h
@@ -25,8 +25,8 @@
 #ifdef _WIN32
 
 #ifdef _XBOX
-#include <xtl.h>
 #include <winsockx.h>
+#include <xtl.h>
 #define snprintf _snprintf
 #define strcasecmp stricmp
 #define strncasecmp strnicmp
@@ -37,76 +37,87 @@
 #include <ws2tcpip.h>
 #endif
 
-#define GetSockError()	WSAGetLastError()
-#define SetSockError(e)	WSASetLastError(e)
-#define setsockopt(a,b,c,d,e)	(setsockopt)(a,b,c,(const char *)d,(int)e)
-#define EWOULDBLOCK	WSAETIMEDOUT	/* we don't use nonblocking, but we do use timeouts */
-#define sleep(n)	Sleep(n*1000)
-#define msleep(n)	Sleep(n)
-#define SET_RCVTIMEO(tv,s)	int tv = s*1000
+#define GetSockError() WSAGetLastError()
+#define SetSockError(e) WSASetLastError(e)
+#define setsockopt(a, b, c, d, e) (setsockopt)(a, b, c, (const char *)d, (int)e)
+#define EWOULDBLOCK \
+    WSAETIMEDOUT /* we don't use nonblocking, but we do use timeouts */
+#define sleep(n) Sleep(n * 1000)
+#define msleep(n) Sleep(n)
+#define SET_RCVTIMEO(tv, s) int tv = s * 1000
 #else /* !_WIN32 */
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/times.h>
-#include <netdb.h>
 #include <arpa/inet.h>
-#include <unistd.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
-#define GetSockError()	errno
-#define SetSockError(e)	errno = e
+#include <sys/socket.h>
+#include <sys/times.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define GetSockError() errno
+#define SetSockError(e) errno = e
 #undef closesocket
-#define closesocket(s)	close(s)
-#define msleep(n)	usleep(n*1000)
-#define SET_RCVTIMEO(tv,s)	struct timeval tv = {s,0}
+#define closesocket(s) close(s)
+#define msleep(n) usleep(n * 1000)
+#define SET_RCVTIMEO(tv, s) struct timeval tv = {s, 0}
 #endif
 
 #include "rtmp.h"
 
 #ifdef USE_POLARSSL
+#include <polarssl/havege.h>
 #include <polarssl/net.h>
 #include <polarssl/ssl.h>
-#include <polarssl/havege.h>
 typedef struct tls_ctx {
-	havege_state hs;
-	ssl_session ssn;
+    havege_state hs;
+    ssl_session ssn;
 } tls_ctx;
 #define TLS_CTX tls_ctx *
-#define TLS_client(ctx,s)	s = malloc(sizeof(ssl_context)); ssl_init(s);\
-	ssl_set_endpoint(s, SSL_IS_CLIENT); ssl_set_authmode(s, SSL_VERIFY_NONE);\
-	ssl_set_rng(s, havege_rand, &ctx->hs); ssl_set_ciphers(s, ssl_default_ciphers);\
-	ssl_set_session(s, 1, 600, &ctx->ssn)
-#define TLS_setfd(s,fd)	ssl_set_bio(s, net_recv, &fd, net_send, &fd)
-#define TLS_connect(s)	ssl_handshake(s)
-#define TLS_read(s,b,l)	ssl_read(s,(unsigned char *)b,l)
-#define TLS_write(s,b,l)	ssl_write(s,(unsigned char *)b,l)
-#define TLS_shutdown(s)	ssl_close_notify(s)
-#define TLS_close(s)	ssl_free(s); free(s)
+#define TLS_client(ctx, s)                   \
+    s = malloc(sizeof(ssl_context));         \
+    ssl_init(s);                             \
+    ssl_set_endpoint(s, SSL_IS_CLIENT);      \
+    ssl_set_authmode(s, SSL_VERIFY_NONE);    \
+    ssl_set_rng(s, havege_rand, &ctx->hs);   \
+    ssl_set_ciphers(s, ssl_default_ciphers); \
+    ssl_set_session(s, 1, 600, &ctx->ssn)
+#define TLS_setfd(s, fd) ssl_set_bio(s, net_recv, &fd, net_send, &fd)
+#define TLS_connect(s) ssl_handshake(s)
+#define TLS_read(s, b, l) ssl_read(s, (unsigned char *)b, l)
+#define TLS_write(s, b, l) ssl_write(s, (unsigned char *)b, l)
+#define TLS_shutdown(s) ssl_close_notify(s)
+#define TLS_close(s) \
+    ssl_free(s);     \
+    free(s)
 
 #elif defined(USE_GNUTLS)
 #include <gnutls/gnutls.h>
 typedef struct tls_ctx {
-	gnutls_certificate_credentials_t cred;
-	gnutls_priority_t prios;
+    gnutls_certificate_credentials_t cred;
+    gnutls_priority_t prios;
 } tls_ctx;
-#define TLS_CTX	tls_ctx *
-#define TLS_client(ctx,s)	gnutls_init((gnutls_session_t *)(&s), GNUTLS_CLIENT); gnutls_priority_set(s, ctx->prios); gnutls_credentials_set(s, GNUTLS_CRD_CERTIFICATE, ctx->cred)
-#define TLS_setfd(s,fd)	gnutls_transport_set_ptr(s, (gnutls_transport_ptr_t)(long)fd)
-#define TLS_connect(s)	gnutls_handshake(s)
-#define TLS_read(s,b,l)	gnutls_record_recv(s,b,l)
-#define TLS_write(s,b,l)	gnutls_record_send(s,b,l)
-#define TLS_shutdown(s)	gnutls_bye(s, GNUTLS_SHUT_RDWR)
-#define TLS_close(s)	gnutls_deinit(s)
+#define TLS_CTX tls_ctx *
+#define TLS_client(ctx, s)                                \
+    gnutls_init((gnutls_session_t *)(&s), GNUTLS_CLIENT); \
+    gnutls_priority_set(s, ctx->prios);                   \
+    gnutls_credentials_set(s, GNUTLS_CRD_CERTIFICATE, ctx->cred)
+#define TLS_setfd(s, fd) \
+    gnutls_transport_set_ptr(s, (gnutls_transport_ptr_t)(long)fd)
+#define TLS_connect(s) gnutls_handshake(s)
+#define TLS_read(s, b, l) gnutls_record_recv(s, b, l)
+#define TLS_write(s, b, l) gnutls_record_send(s, b, l)
+#define TLS_shutdown(s) gnutls_bye(s, GNUTLS_SHUT_RDWR)
+#define TLS_close(s) gnutls_deinit(s)
 
-#else	/* USE_OPENSSL */
-#define TLS_CTX	SSL_CTX *
-#define TLS_client(ctx,s)	s = SSL_new(ctx)
-#define TLS_setfd(s,fd)	SSL_set_fd(s,fd)
-#define TLS_connect(s)	SSL_connect(s)
-#define TLS_read(s,b,l)	SSL_read(s,b,l)
-#define TLS_write(s,b,l)	SSL_write(s,b,l)
-#define TLS_shutdown(s)	SSL_shutdown(s)
-#define TLS_close(s)	SSL_free(s)
+#else /* USE_OPENSSL */
+#define TLS_CTX SSL_CTX *
+#define TLS_client(ctx, s) s = SSL_new(ctx)
+#define TLS_setfd(s, fd) SSL_set_fd(s, fd)
+#define TLS_connect(s) SSL_connect(s)
+#define TLS_read(s, b, l) SSL_read(s, b, l)
+#define TLS_write(s, b, l) SSL_write(s, b, l)
+#define TLS_shutdown(s) SSL_shutdown(s)
+#define TLS_close(s) SSL_free(s)
 
 #endif
 #endif
